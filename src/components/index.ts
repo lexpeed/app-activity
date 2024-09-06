e5ed492111685f78721a6a3fe4e69d21b058d503
Eros Mendonça <erosmendonca@outlook.com>
2024-09-06 18:13:38 +0100

    feat: initial footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./button";
+export { default as Footer } from "./footer";
 export { default as Header } from "./header";
 export { default as KnowledgeAreaCard } from "./knowledge-area-card";
