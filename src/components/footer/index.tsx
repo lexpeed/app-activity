1db0ce1b7ef57715a394253bcf14822cf07f0957
Eros Mendonça <erosmendonca@outlook.com>
2024-09-10 01:39:44 +0100

    feat: improving footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="px-4 py-8 mt-4 text-center bg-gray-200">
       <p>© 2024 - All rights reserved</p>
     </footer>
   );
