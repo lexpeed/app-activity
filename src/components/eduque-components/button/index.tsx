529c2577a206f206da58468c2dc4476da8c51d67
Eros Mendonça <erosmendonca@outlook.com>
2024-09-15 16:04:18 +0100

    feat: adding button to eduque components

---
+++ src/components/eduque-components/button/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+type Colors = "primary" | "secondary" | "tertiary" | "white" | "black";
+// Maybe we can use the tailwind config to get the colors
+// and use an algorithm to determine the text color
+
+export interface Props {
+  children: React.ReactNode;
+  color?: Colors;
+  textColor?: Colors;
+  onClick?: () => void;
+}
+
+const commonClasses = "rounded-sm px-4 py-1.5";
+const hoverClasses = "hover:opacity-90";
+const clickClasses = "active:opacity-100";
+
+const Button = ({ children, color, textColor, onClick }: Props) => {
+  const colorClass = `bg-${color ?? "primary"}`;
+  const textColorClass = `text-${textColor ?? "white"}`;
+  return (
+    <button
+      onClick={onClick}
+      className={`
+        ${colorClass}
+        ${textColorClass}
+        ${commonClasses}
+        ${hoverClasses}
+        ${clickClasses}
+    `}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
