4cdcf33e1924d4916bcc17ac80294d575f0b4732
Eros Mendonça <erosmendonca@outlook.com>
2024-09-18 22:46:31 +0100

    feat: simple popper component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { default as ActivityResultCard } from "./activity-result-card";
 export { default as Footer } from "./footer";
 export { default as Header } from "./header";
 export { default as KnowledgeAreaCard } from "./knowledge-area-card";
+export { default as Popper } from "./popper";
 export { default as Providers } from "./providers";
---
+++ src/components/popper/index.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface Props {
+  children: React.ReactNode;
+  anchorEl: HTMLElement | null;
+  isOpen: boolean;
+}
+
+const Popper = ({ children, anchorEl, isOpen }: Props) => {
+  const [position, setPosition] = useState({ top: 0, left: 0 });
+
+  useEffect(() => {
+    if (anchorEl && isOpen) {
+      const { top, left, height, width } = anchorEl.getBoundingClientRect();
+      const arrowSize = 8;
+      const newLeft = left + width / 2;
+      const newTop = top + height + window.scrollY + arrowSize * 2;
+      setPosition({ top: newTop, left: newLeft });
+    }
+  }, [anchorEl, isOpen]);
+
+  return (
+    <div
+      className={`
+        bg-gray-50 shadow-lg rounded-sm
+        absolute z-10
+        transition-all duration-300
+        transform -translate-x-1/2
+        ${isOpen ? "opacity-100" : "opacity-0 pointer-events-none"}
+        ${isOpen ? "scale-100" : "scale-95"}
+      `}
+      style={{
+        top: position.top,
+        left: position.left,
+      }}
+    >
+      <div
+        className={`
+          w-0 h-0
+          border-x-8 border-x-transparent
+          border-b-8 border-b-gray-50
+          absolute top-0 left-1/2
+          transform -translate-x-1/2 -translate-y-full
+        `}
+      ></div>
+      {children}
+    </div>
+  );
+};
+
+export default Popper;
