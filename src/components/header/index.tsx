c983ca5226d406a5cb8c7b4c1fd3cc64f1b59a2b
pedropereiralxp <pedro.pereira@lexpeed.com.br>
2024-09-18 21:35:33 -0300

    chore: updates

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,6 @@ import ProfileButton from "../profile-button";
 
 const Header = () => {
   const auth = useAuth();
-  console.log(auth);
 
   return (
     <header className="bg-primary p-6 flex flex-col gap-8 items-center">
